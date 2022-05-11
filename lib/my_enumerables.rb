20dc0c3e65d59a617c7289799dc95c841f03041f
justinkim2015 <justinkim2015@gmail.com>
2022-05-11 10:38:34 +0900

    Finish eachwith index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,26 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    i = 0
+    my_each do |value|
+      yield value, i
+      i += 1
+    end
+  end
+
+  def my_select
+  end 
 end
 
+# bundle exec rspec spec/my_each_with_index_spec.rb
 # You will first have to define my_each
 # on the Array class. Methods defined in
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for value in self do
+      yield value
+    end
+  end
 end
+
