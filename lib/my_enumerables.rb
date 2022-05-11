ebc1c0d16d00926c420c084557a6896d05f55c7b
justinkim2015 <justinkim2015@gmail.com>
2022-05-11 11:07:02 +0900

    Finish my_select

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,10 +8,17 @@ module Enumerable
   end
 
   def my_select
-  end 
+    final_array = []
+    my_each do |value|
+      final_array << value if (yield value) == true
+    end
+    final_array
+  end
+
+  
 end
 
-# bundle exec rspec spec/my_each_with_index_spec.rb
+# bundle exec rspec spec/my_select_spec.rb
 # You will first have to define my_each
 # on the Array class. Methods defined in
 # your enumerable module will have access
@@ -24,3 +31,5 @@ class Array
   end
 end
 
+enumerable = [4,23,5,21,3,2,1,8]
+enumerable.my_select { |value| value > 10 }
