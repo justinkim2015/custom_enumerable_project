a029edc4fd5552d94740b584afa19b866cd6e8c3
justinkim2015 <justinkim2015@gmail.com>
2022-05-11 12:12:21 +0900

    Finish all enumerables

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -15,16 +15,6 @@ module Enumerable
     final_array
   end
 
-  # def my_all?
-  #   my_each do |value|
-  #     if (yield value) == true
-  #       true
-  #     else
-  #       return false
-  #     end
-  #   end
-  # end
-
   def my_all?
     array = []
     my_each do |value|
@@ -36,9 +26,63 @@ module Enumerable
       false
     end
   end
+
+  def my_any?
+    array = []
+    my_each do |value|
+      array << (yield value)
+    end
+    if array.include?(true)
+      true
+    else
+      false
+    end
+  end
+
+  def my_none?
+    array = []
+    my_each do |value|
+      array << (yield value)
+    end
+    if !array.include?(true)
+      true
+    else
+      false
+    end
+  end
+
+  def my_count
+    count = 0
+    if block_given?
+      my_each do |value|
+        count += 1 if (yield value) == true
+      end
+    else
+      my_each do
+        count += 1
+      end
+    end
+    count
+  end
+
+  def my_map
+    array = []
+    my_each do |value|
+      array << (yield value)
+    end
+    array
+  end
+
+  def my_inject(initial_value = 0)
+    sum = initial_value
+    my_each do |value|
+      sum = (yield value, sum)
+    end
+    sum
+  end
 end
 
-# bundle exec rspec spec/my_all_spec.rb
+# bundle exec rspec spec/my_inject_spec.rb
 # You will first have to define my_each
 # on the Array class. Methods defined in
 # your enumerable module will have access
@@ -52,4 +96,4 @@ class Array
 end
 
 enumerable = [1, 1, 2, 3, 5, 8, 13, 21, 34]
-enumerable.my_all? { |value| value > 0 }
+enumerable.my_inject(0) { |sum, value| sum + value }
